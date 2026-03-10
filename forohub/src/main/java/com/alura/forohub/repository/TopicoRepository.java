f8ef69bdedaa36918b1bd4a4bbb42a9ebd58967e
BioMarcela <97031209+BioMarcela@users.noreply.github.com>
2026-03-10 11:51:39 -0300

    Delete forohub directory
    Borrar archivo antiguo

--- forohub/src/main/java/com/alura/forohub/repository/TopicoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.alura.forohub.repository;
-
-import com.alura.forohub.model.Topico;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TopicoRepository extends JpaRepository<Topico, Long> {
-}
